ceb9449877c3fd0f74fc2e70a301ea0435c1c82a
Thabo <tkumalo.dev@gmail.com>
2025-05-16 14:24:38 +0200

    remove duplcate files

--- ChannelZero_Repo_1/Claim Link mrk 5/log-claim.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btn = document.getElementById("apply-btn");
-    const target = document.getElementById("log-claim");
-
-    btn.addEventListener("click", function (e) {
-      e.preventDefault();
-      target.scrollIntoView({ behavior: "smooth" });
-    });
-  });
